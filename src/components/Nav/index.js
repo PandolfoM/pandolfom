626f2567e535f20982bad4d3e20dde5d145b8ba8
Matthew Pandolfo <matt@pandolfo.com>
2022-04-22 21:54:43 -0400

    navbar with style

---
+++ src/components/Nav/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import './style.css'
+
+function Navigation() {
+  return (
+    <Navbar variant="dark">
+      <Container>
+        <Navbar.Brand>Portfolio</Navbar.Brand>
+        <Nav className="justify-content-end">
+          <Nav.Link>Home</Nav.Link>
+          <Nav.Link>About</Nav.Link>
+          <Nav.Link>Work</Nav.Link>
+          <Nav.Link>Contact</Nav.Link>
+          <Nav.Link>Resume</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default Navigation;
