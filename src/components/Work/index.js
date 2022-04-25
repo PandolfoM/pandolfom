7df7816bd05b706b41874ef82bac864f58947fbf
Matthew Pandolfo <matt@pandolfo.com>
2022-04-25 18:02:30 -0400

    work section

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,59 +1,109 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card, Button, Container, Row, Col } from "react-bootstrap";
 import "./style.css";
+import repo from "../../config.json";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 function Work() {
+  const [page, setpage] = useState(0);
+
   return (
     <Container fluid className="worksection">
       <Row>
-        <Col lg={3} md={2} xs={1} />
+        <Col lg={3} md={2} xs={1} className="workLeft">
+          {page === 1 && (
+            <FontAwesomeIcon
+              icon={faAngleLeft}
+              onClick={(e) => {
+                console.log("pressed");
+                setpage(0);
+              }}
+            />
+          )}
+        </Col>
         <Col>
           <Row>
-            <Col className="workcards">
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="https://picsum.photos/286/180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="outline-primary">View Site</Button>
-                  <Button variant="outline-primary">View on GitHub</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="workcards">
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="https://picsum.photos/286/180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="outline-primary">View Site</Button>
-                  <Button variant="outline-primary">View on GitHub</Button>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col className="workcards">
-              <Card style={{ width: "18rem" }}>
-                <Card.Img variant="top" src="https://picsum.photos/286/180" />
-                <Card.Body>
-                  <Card.Title>Card Title</Card.Title>
-                  <Card.Text>
-                    Some quick example text to build on the card title and make
-                    up the bulk of the card's content.
-                  </Card.Text>
-                  <Button variant="outline-primary">View Site</Button>
-                  <Button variant="outline-primary">View on GitHub</Button>
-                </Card.Body>
-              </Card>
-            </Col>
+            {page === 0 ? (
+              <>
+                {repo.slice(0, 3).map((repo) => (
+                  <Col key={repo.site} className="workcards">
+                    <Card style={{ width: "18rem" }}>
+                      <Card.Img
+                        className="cardImg"
+                        variant="top"
+                        alt={repo.title}
+                        src={repo.image}
+                      />
+                      <Card.Body>
+                        <Card.Title>{repo.title}</Card.Title>
+                        <Card.Text className="workDesc">{repo.description}</Card.Text>
+                      </Card.Body>
+                      <Card.Footer className="workFooter">
+                        <Button
+                          variant="outline-primary"
+                          href={repo.site}
+                          target="_blank">
+                          View Site
+                        </Button>
+                        <Button
+                          variant="outline-primary"
+                          href={repo.github}
+                          target="_blank">
+                          View on GitHub
+                        </Button>
+                      </Card.Footer>
+                    </Card>
+                  </Col>
+                ))}
+              </>
+            ) : (
+              <>
+                {repo.slice(3, 6).map((repo) => (
+                  <Col key={repo.site} className="workcards">
+                    <Card style={{ width: "18rem" }}>
+                      <Card.Img
+                        className="cardImg"
+                        variant="top"
+                        alt={repo.title}
+                        src={repo.image}
+                      />
+                      <Card.Body>
+                        <Card.Title>{repo.title}</Card.Title>
+                        <Card.Text className="workDesc">{repo.description}</Card.Text>
+                      </Card.Body>
+                      <Card.Footer className="workFooter">
+                        <Button
+                          variant="outline-primary"
+                          href={repo.site}
+                          target="_blank">
+                          View Site
+                        </Button>
+                        <Button
+                          variant="outline-primary"
+                          href={repo.github}
+                          target="_blank">
+                          View on GitHub
+                        </Button>
+                      </Card.Footer>
+                    </Card>
+                  </Col>
+                ))}
+              </>
+            )}
           </Row>
         </Col>
-        <Col lg={3} md={2} xs={1} />
+        <Col lg={3} md={2} xs={1} className="workRight">
+          {page === 0 && (
+            <FontAwesomeIcon
+              icon={faAngleRight}
+              onClick={(e) => {
+                console.log(page);
+                setpage(1);
+              }}
+            />
+          )}
+        </Col>
       </Row>
     </Container>
   );
