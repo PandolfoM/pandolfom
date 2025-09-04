89bffd2d3332ce19a59f0001b9e9881b1f3c6420
Matthew Pandolfo <matt@pandolfo.com>
2025-09-04 07:49:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,5 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-      alias: [
-      {
-        find: './runtimeConfig',
-        replacement: './runtimeConfig.browser',
-      },
-    ]
-  },
-  build: {
-    outDir: "build",
-  },
-  base: "./",
+  plugins: [react()]
 });
